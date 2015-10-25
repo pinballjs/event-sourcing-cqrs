d99c47198390b005f1f15a35207651bee1a109e0
Alberto Miorin <github@ululi.it>
2015-10-25 18:26:44 +0100

    add money_transfer aggregate

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 'use strict';
 
+let promise = require('bluebird');
 let co = require('co');
-let pinball = require('pinball')('example');
-let es = require('./lib/event_store')();
-let as = require('./lib/account_service');
-let print = require('./lib/utils').print;
-require('colors');
 
-pinball.use('eventemitter')
-       .add({ role:'eventstore', cmd:'save' }, es.save)
-       .add({ role:'eventstore', cmd:'find' }, es.find)
-       .add({ role:'eventstore', cmd:'update' }, es.update)
-       .add({ role:'account', cmd:'open' }, as.openAccountService)
-       .add({ role:'account' }, as.accountService);
+let pinball = require('pinball')('example');
+pinball.use('eventemitter');
+let es = require('./lib/event_store')(pinball);
+require('./lib/account_service')(pinball);
+require('./lib/money_transfer_service')(pinball);
 
 co(function *main() {
-  let reply = yield pinball.act({
+  let acc1 = yield pinball.act({
     role: 'account',
     cmd: 'open',
-    initialBalance: 100
+    initialBalance: 0
   });
   yield pinball.act({
     role: 'account',
     cmd: 'credit',
-    guid: reply.guid,
+    guid: acc1.guid,
     amount: 200
   }, 1000, 1);
-  yield pinball.act({
+  let acc2 = yield pinball.act({
     role: 'account',
-    cmd: 'credit',
-    guid: reply.guid,
-    amount: 100
-  }, 1000, 1);
+    cmd: 'open',
+    initialBalance: 10
+  });
   yield pinball.act({
-    role: 'account',
-    cmd: 'debit',
-    guid: reply.guid,
-    amount: 50
-  }, 1000, 1);
-  print(es.getStore());
+    role: 'moneyTransfer',
+    cmd: 'create',
+    from: acc1.guid,
+    to: acc2.guid,
+    amount: 30
+  });
+  yield promise.delay(500);
+
+  let print = require('./lib/utils').print;
+  print(es._getStore());
+  print(es._getEvents());
 }).catch(function(err) {
   console.log(err.stack);
 });
-
-
